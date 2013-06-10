f40402410a7e79c82794fa67b4bad98992c6e0ce
Nur Hossain <nur@Nur-Hossains-MacBook-Pro.local>
2013-06-11 01:46:39 +0600

    finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html do 
+          flash[:success] = 'Welcome to the Sample App!'
+          redirect_to @user
+        end
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
@@ -60,7 +63,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html do 
+          flash[:success] = 'User was successfully updated!' 
+          redirect_to @user
+        end
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
